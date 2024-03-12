d7f3e4ebd228bd353c46fef2d25ee6e7219acc4f
eminhologlu <215541008@firat.edu.tr>
2024-03-12 16:30:03 +0300

    anasayfa eklendi

---
+++ lib/anasayfa.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class Anasayfa extends StatefulWidget {
+  const Anasayfa({super.key});
+
+  @override
+  State<Anasayfa> createState() => _AnasayfaState();
+}
+
+class _AnasayfaState extends State<Anasayfa> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 25, 139, 28),
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(height: MediaQuery.of(context).size.height*0.13),
+            GestureDetector(
+              onTap: () {},
+              child: buildContainer("Öğrenci Toplulukları")),
+            SizedBox(height: MediaQuery.of(context).size.height*0.05),
+            GestureDetector(
+              onTap: () {},
+              child: buildContainer("Üyesi Olduğun Topluluklar")),
+            SizedBox(height: MediaQuery.of(context).size.height*0.05),
+            GestureDetector(
+              onTap: () {},
+              child: buildContainer("Yönetici Olduğun Topluluklar")),
+            SizedBox(height: MediaQuery.of(context).size.height*0.05),
+            GestureDetector(
+              onTap: () {},
+              child: buildContainer("Ayarlar")),
+          ],
+        ),
+      ),
+    );
+  }
+
+Widget buildContainer(String text){
+  return Container(
+    child: Center(child: Text(text,style: TextStyle(fontSize: MediaQuery.of(context).size.width*0.05, fontFamily: "Lalezar", color: Colors.white),)),
+              height: MediaQuery.of(context).size.height*0.15,
+              width: MediaQuery.of(context).size.width*0.7,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(MediaQuery.of(context).size.width*0.05),
+                color: Colors.black
+              ));
+}
+
+}
