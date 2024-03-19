4ea06b1567cb46108b6180005869b5ef8e4c98fd
eminhologlu <215541008@firat.edu.tr>
2024-03-19 14:27:43 +0300

    topluluklar sayfasındaki sorunlar düzeltildi.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:kulup/loading.dart';
 import 'package:kulup/login.dart';
-import 'package:kulup/topluluklar.dart';
-
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(const MyApp());
 }
 
@@ -15,7 +19,7 @@ class MyApp extends StatelessWidget {
       title: 'Shequ',
       debugShowCheckedModeBanner: false,
       home: FutureBuilder(
-        future: Future.delayed(Duration(seconds: 0)), // 5 saniye bekle
+        future: Future.delayed(Duration(seconds: 3)), // 5 saniye bekle
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return LoadingScreen(); // Bekleme ekranını göster
--- lib/topluluklar.dart
+++ lib/topluluklar.dart
@@ -1,7 +1,6 @@
-import 'package:flutter/cupertino.dart';
+import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter/services.dart';
 
 class Topluluklar extends StatefulWidget {
   const Topluluklar({super.key});
@@ -11,30 +10,36 @@ class Topluluklar extends StatefulWidget {
 }
 
 class _TopluluklarState extends State<Topluluklar> {
+  late List<String> topluluklar = [];
+  late Map<String, String> logolar = {};
+  late List<String> filteredTopluluklar = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadTopluluklar();
+  }
+
+  Future<void> _loadTopluluklar() async {
+    String jsonString =
+        await rootBundle.loadString('assets/json/topluluklar.json');
+    Map<String, dynamic> data = json.decode(jsonString);
+    List<String> topluluklarList = [];
+    Map<String, String> logolarMap =
+        {}; // Logo URL'lerini saklamak için boş bir Map oluşturuyoruz
+    data['Sayfa1'].forEach((key, value) {
+      topluluklarList.add(value['TOPLULUKLAR']);
+      logolarMap[value['TOPLULUKLAR']] = value['LOGO'];
+    });
+    setState(() {
+      topluluklar = topluluklarList;
+      logolar = logolarMap; // Oluşturduğumuz Map'i setState içinde atıyoruz
+      filteredTopluluklar = topluluklar;
+    });
+  }
+
   final TextEditingController searchController = TextEditingController();
-  final List<String> itemList = [
-    'Item 1',
-    'Item 2',
-    'Item 3',
-    'Item 4',
-    'Item 5',
-    'Item 6',
-    'Item 7',
-    'Item 8',
-    'Item 9',
-    'Item 10',
-    'Item 11',
-    'Item 2',
-    'Item 3',
-    'Item 4',
-    'Item 5',
-    'Item 6',
-    'Item 7',
-    'Item 8',
-    'Item 9',
-    'Item 10',
-    'Item 11',
-  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -44,13 +49,16 @@ class _TopluluklarState extends State<Topluluklar> {
         child: Scaffold(
             appBar: AppBar(
               leading: GestureDetector(
-                onTap: () {
-                  Navigator.pop(context);
-                },
-                child: Icon(Icons.arrow_back_ios_new_rounded,color: Colors.white,)),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Icon(
+                    Icons.arrow_back_ios_new_rounded,
+                    color: Colors.white,
+                  )),
               flexibleSpace: Container(
-                decoration:
-                    BoxDecoration(color: Color.fromARGB(255, 25, 139, 28)),
+                decoration: const BoxDecoration(
+                    color: Color.fromARGB(255, 25, 139, 28)),
               ),
               title: TextField(
                 cursorHeight: MediaQuery.of(context).size.height * 0.02,
@@ -63,37 +71,59 @@ class _TopluluklarState extends State<Topluluklar> {
                 decoration: InputDecoration(
                   prefixIcon: GestureDetector(
                       onTap: () {},
-                      child: Icon(Icons.search_rounded, color: Colors.white)),
+                      child: const Icon(Icons.search_rounded,
+                          color: Colors.white)),
                   hintText: 'Topluluk ara...',
                   hintStyle: TextStyle(
-                      color: Color.fromARGB(137, 255, 255, 255),
+                      color: const Color.fromARGB(137, 255, 255, 255),
                       height:
                           MediaQuery.of(context).size.height * 0.00000000001),
                   border: InputBorder.none,
                 ),
-                onChanged: (value) {},
+                onChanged: (value) {
+                  setState(() {
+                    filteredTopluluklar = _filterTopluluklar(value);
+                  });
+                },
               ),
             ),
-            backgroundColor: Color.fromARGB(255, 25, 139, 28),
+            backgroundColor: const Color.fromARGB(255, 25, 139, 28),
             body: ListView.separated(
-              padding: EdgeInsets.all(MediaQuery.of(context).size.width*0.03),
-              itemCount: itemList.length,
+              padding: EdgeInsets.all(MediaQuery.of(context).size.width * 0.03),
+              itemCount: filteredTopluluklar.length,
               itemBuilder: (BuildContext context, int index) {
+                final toplulukAdi = filteredTopluluklar[index];
+                String? logoURL = logolar[toplulukAdi];
                 return Container(
-                  height: MediaQuery.of(context).size.height*0.1,
+                  height: MediaQuery.of(context).size.height * 0.1,
                   decoration: BoxDecoration(
-                    color: Colors.white,
-                    border: Border.all(color: Colors.white),
-                    borderRadius: BorderRadius.circular(MediaQuery.of(context).size.width*0.05)
-                  ),
+                      color: Colors.white,
+                      border: Border.all(color: Colors.white),
+                      borderRadius: BorderRadius.circular(
+                          MediaQuery.of(context).size.width * 0.05)),
                   child: Padding(
-                    padding: EdgeInsets.only(left: MediaQuery.of(context).size.width*0.01),
+                    padding: EdgeInsets.only(
+                        left: MediaQuery.of(context).size.width * 0.03),
                     child: Row(
                       children: [
-                        CircleAvatar(backgroundColor: Color.fromARGB(255, 25, 139, 28),radius: MediaQuery.of(context).size.width*0.1,),
+                        CircleAvatar(
+                          foregroundImage: logoURL != null && logoURL != "nan"
+                              ? NetworkImage(logoURL)
+                              : NetworkImage(
+                                  "https://unievi.firat.edu.tr/assets/front/img/firat-logo-yeni.png"),
+                          backgroundColor: Colors.white,
+                          radius: MediaQuery.of(context).size.width * 0.085,
+                        ),
                         Padding(
-                          padding: EdgeInsets.only(left: MediaQuery.of(context).size.width*0.03),
-                          child: Text('Entry LKASDLKŞ\nASJDKgghghŞL KAJDL ${itemList[index]}'),
+                          padding: EdgeInsets.only(
+                              left: MediaQuery.of(context).size.width * 0.03),
+                          child: Container(
+                            width: MediaQuery.of(context).size.width * 0.65,
+                            child: Text(
+                              '${toplulukAdi}',
+                              overflow: TextOverflow.visible,
+                            ),
+                          ),
                         ),
                       ],
                     ),
@@ -105,5 +135,12 @@ class _TopluluklarState extends State<Topluluklar> {
             )),
       ),
     );
+  }
+
+  List<String> _filterTopluluklar(String query) {
+    query = query.toLowerCase();
+    return topluluklar
+        .where((toplulukAdi) => toplulukAdi.toLowerCase().contains(query))
+        .toList();
   }
 }
